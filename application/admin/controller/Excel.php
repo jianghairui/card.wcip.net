d520d0910a15dd8faa0a65bfb4ec3b8b49ad4497
Jiang <1873645345@qq.com>
2020-03-20 15:48:58 +0800

    完成100% 待测试交接

--- application/admin/controller/Excel.php
+++ application/admin/controller/Excel.php
@@ -122,4 +122,77 @@ class Excel extends Base {
     }
 
 
+    public function orderList() {
+        $list = [
+            ['name'=>'Jianghairui','age'=>28],
+            ['name'=>'Hanchi','age'=>28],
+            ['name'=>'Ganrongjian ','age'=>25],
+        ];
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setTitle('掌控的历史统计');
+
+        $sheet->getColumnDimension('A')->setWidth(30);
+        $sheet->getRowDimension('1')->setRowHeight(30);
+//        $sheet->getColumnDimension('B')->setWidth(12);
+//        $sheet->getColumnDimension('C')->setWidth(12);
+//        $sheet->getColumnDimension('D')->setWidth(12);
+//        $sheet->getColumnDimension('E')->setWidth(12);
+//
+//        $sheet->getStyle('A:Z')->applyFromArray([
+//            'alignment' => [
+//                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+//                'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+//            ],
+//        ]);
+//        $sheet->getStyle('A1')->applyFromArray([
+//            'alignment' => [
+//                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+//            ],
+//        ]);
+//
+//        $sheet->getStyle('A')->getNumberFormat()->setFormatCode( \PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_NUMBER);
+//        $sheet->getStyle('C:D')->getNumberFormat()->setFormatCode( \PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_NUMBER_00);
+//
+//        $sheet->mergeCells('A1:E1');
+
+        $sheet->setCellValue('A1', '掌控的历史销售统计' . date('Y-m-d H:i:s') . ' 制表人:' . session('username'));
+        $sheet->setCellValue('A2', '设备名');
+//        $sheet->setCellValue('B2', '设备号');
+//        $sheet->setCellValue('C2', '销售额(元)');
+//        $sheet->setCellValue('D2', '单价(元)');
+//        $sheet->setCellValue('E2', '余量(包)');
+//        $sheet->getStyle('A2:E2')->getFont()->setBold(true);
+
+        $index = 3;
+        foreach ($list as $v) {
+            $sheet->setCellValue('A'.$index, $v['name']);
+//            $sheet->setCellValue('B'.$index, $v['device_num']);
+//            $sheet->setCellValue('C'.$index, $v['total_price']);
+//            $sheet->setCellValue('D'.$index, $v['unit_price']);
+//            $sheet->setCellValue('E'.$index, $v['stock']);
+            $index++;
+        }
+
+//        $sheet->setCellValue('C'.$index, $total_sum);
+
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');//告诉浏览器输出07Excel文件
+//header(‘Content-Type:application/vnd.ms-excel‘);//告诉浏览器将要输出Excel03版本文件
+        header('Content-Disposition: attachment;filename="user'.date('Y-m-d').'.xlsx"');//告诉浏览器输出浏览器名称
+        header('Cache-Control: max-age=0');//禁止缓存
+
+        $writer = new Xlsx($spreadsheet);
+        $writer->save('php://output');
+    }
+
+    public function userList() {
+
+    }
+
+    public function fansList() {
+
+    }
+
+
 }
